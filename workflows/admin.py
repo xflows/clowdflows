a8df36fc56ea001d47f8af4c2e775355b2365d0d
matjaz <matjaz@jursic.si>
2012-09-28 03:11:10 +0200

    Small (productivity) improvements of admin sites: - "Save as new" button to all admin interfaces - admin buttons also on the top of the page - search button on top of admin lists (always searches by name)

--- workflows/admin.py
+++ workflows/admin.py
@@ -5,6 +5,10 @@ from django.db import models
 
 from orderable_inlines import OrderableTabularInline
 
+admin.ModelAdmin.save_as = True; #adds "Save as new" button to all admin interfaces
+admin.ModelAdmin.save_on_top = True; #adds admin buttons also on the top of the page
+admin.ModelAdmin.search_fields = ['name'] #adds search button on top of admin lists (allways searches by name)
+
 class OrderableTabularInlineStyled(OrderableTabularInline):
     formfield_overrides = {
         models.CharField: {'widget': TextInput(attrs={'size':10})},
