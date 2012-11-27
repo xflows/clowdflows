1ab15256ed018609008236dbdc18a0d614c1b84f
matjaz <matjaz@jursic.si>
2012-11-27 05:58:13 +0100

    Nl_toolkit package modifications.

--- workflows/nl_toolkit/library.py
+++ workflows/nl_toolkit/library.py
@@ -1,5 +1,18 @@
 import re
 
+def nl_toolkit_get_all_synsets(input_dict):
+    if input_dict['corpus'].lower()=='wordnet':
+        from nltk.corpus import wordnet as wn
+        synsets = {}
+        for word in input_dict['words']:
+            synsets[word] = wn.synsets(word)
+        return {'synsets' : synsets}
+
+def nl_toolkit_get_word_synsets(input_dict):
+    if input_dict['corpus'].lower()=='wordnet':
+        from nltk.corpus import wordnet as wn
+        return {'synsets':wn.synsets(input_dict['word'])}
+
 def nl_toolkit_create_integers(input_dict):
     intStr = input_dict['intStr']
     intList = []
