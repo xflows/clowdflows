357a9a7a1bbe10d6a1b16d5ad594f384f3a9db6a
Anze Vavpetic <anze.vavpetic@ijs.si>
2012-11-29 15:25:19 +0100

    don't automatically import SD and bio packages

--- workflows/bioinformatics/settings.py
+++ workflows/bioinformatics/settings.py
@@ -6,6 +6,6 @@ package_root = os.path.dirname(__file__)
 package_statics = os.path.join(os.path.dirname(__file__), 'static', package_name)
 package_bin = os.path.join(package_root, 'bin')
 
-auto_update_db = True
+auto_update_db = False
 create_backups = True
 
