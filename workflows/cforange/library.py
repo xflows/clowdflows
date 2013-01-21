691abe68b976939f10e11a6d955b0f1c6710a911
Janez K <janez.kranjc@gmail.com>
2013-01-21 13:53:55 +0100

    dodal split dataset v orange package

--- workflows/cforange/library.py
+++ workflows/cforange/library.py
@@ -1,33 +1,12 @@
 import re
 
-def cforange_create_integers(input_dict):
-    intStr = input_dict['intStr']
-    intList = []
-    for i in re.findall(r'\w+', intStr):
-        try:
-            intList.append(int(i))
-        except:
-            pass
-    if input_dict['sort'].lower() == "true":
-        intList.sort()
-    return {'intList':intList}
-
-def cforange_sum_integers(input_dict):
-    intList = input_dict['intList']
-    return {'sum':sum(intList)}
-
-def cforange_pre_filter_integers(input_dict):
-    return input_dict
-
-def cforange_post_filter_integers(postdata,input_dict,output_dict):
-    intListOut = postdata['intListOut']
-    intList = []
-    for i in intListOut:
-        try:
-            intList.append(int(i))
-        except:
-            pass
-    return {'intList': intList}
-
-def cforange_pre_display_summation(input_dict):
-    return {}
+def cforange_split_dataset(input_dict):
+    import orange
+    output_dict = {}
+    data = input_dict['dataset']
+    selection = orange.MakeRandomIndices2(data,float(input_dict['p']))
+    train_data = data.select(selection,0)
+    test_data = data.select(selection,1)
+    output_dict['train_data']=train_data
+    output_dict['test_data']=test_data
+    return output_dict
