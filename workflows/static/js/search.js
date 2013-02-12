1e4546e2bb655726b8c44d65cfee8721ba3260db
bogdan <bogdan1514@gmail.com>
2013-02-12 10:26:06 +0100

    Widget Search function added, fully functional

---
+++ workflows/static/js/search.js
@@ -0,0 +1,17 @@
+jQuery.expr[':'].Contains = function(a, i, m) {
+    return jQuery(a).text().toUpperCase().indexOf(m[3].toUpperCase()) >= 0;
+};
+
+$(function() {
+    $('#searchBox').on('keyup', function() {
+        var w = $(this).val();
+        if (w) {
+            $('#corewidgets li').hide();
+            $('#corewidgets .folder:Contains('+w+')').parent().show().find('li').show();
+            $('#corewidgets li:Contains('+w+')').show();
+        } else {
+            $('#corewidgets li').show();
+        }
+    });
+
+});​
