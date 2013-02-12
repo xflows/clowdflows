c56a004197dcad90bd4b7943a4131236428c7f8e
Anze Vavpetic <anze.vavpetic@ijs.si>
2013-02-12 14:23:53 +0100

    removed an invisible character that caused a js syntax error in chrome

--- workflows/static/js/search.js
+++ workflows/static/js/search.js
@@ -1,25 +1,16 @@
-
-
-
-
-
-
-
-
-jQuery.expr[':'].Contains = function(a, i, m) {
+jQuery.expr[':'].Contains = function (a, i, m) {
     return jQuery(a).text().toUpperCase().indexOf(m[3].toUpperCase()) >= 0;
 };
 
-$(function() {
-    $('#searchBox').on('keyup', function() {
+$(function () {
+    $('#searchBox').on('keyup', function () {
         var w = $(this).val();
         if (w) {
             $('#corewidgets li').hide();
-            $('#corewidgets .folder:Contains('+w+')').parent().show().find('li').show();
-            $('#corewidgets li:Contains('+w+')').show();
+            $('#corewidgets .folder:Contains(' + w + ')').parent().show().find('li').show();
+            $('#corewidgets li:Contains(' + w + ')').show();
         } else {
             $('#corewidgets li').show();
         }
     });
-
-});​
+});
