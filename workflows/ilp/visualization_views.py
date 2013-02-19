3f0439c287e79ac39dc73cf027437d4136f5c0bf
Anze Vavpetic <anze.vavpetic@ijs.si>
2013-02-19 11:03:05 +0100

    added sdmaleph widget; updated sdmsegs viewer to use simpler result dictionary and to use DataTable.js

--- workflows/ilp/visualization_views.py
+++ workflows/ilp/visualization_views.py
@@ -2,19 +2,4 @@ from django.shortcuts import render
 import json
 
 def ilp_sdmsegs_rule_viewer(request,input_dict,output_dict,widget):
-    d = json.loads(input_dict['sdmsegs_rules'])
-    output = {}
-    for k, v in d['A']['WRAcc'].items():
-        terms = []
-        for t in v['terms']:
-            try:
-                terms.append(d['ontDict'][t])
-            except Exception, e:
-                pass
-        output[int(k)] = {
-            'name': terms,
-            'topGenes': int(len(v['topGenes'])),
-            'allGenes': int(len(v['allGenes'])),
-            'wracc': round(v['scores']['wracc'], 3)
-        }
-    return render(request, 'visualizations/sdmsegs_viewer.html',{'widget':widget,'input_dict':input_dict,'rules':output})
+    return render(request, 'visualizations/sdmsegs_viewer.html',{'widget':widget,'rules':json.loads(input_dict['sdmsegs_rules'])})
