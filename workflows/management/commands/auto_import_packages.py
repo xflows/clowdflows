ccc908925002b19968ae75b2541ac243a7427037
matjaz <matjaz@jursic.si>
2013-03-10 07:01:17 +0100

    auto_imort_packages has a new option ('-a', '--ask') to enable it to interactively ask to import packages which are marked not to be imported.

--- workflows/management/commands/auto_import_packages.py
+++ workflows/management/commands/auto_import_packages.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import os
+import sys
 from django.core.management.base import BaseCommand, CommandError
 from workflows import module_importer
 from workflows.management.commands import export_package
@@ -7,7 +8,6 @@ from workflows.management.commands import import_package
 from optparse import make_option
 
 class Command(BaseCommand):
-    args = 'file_name'
     help = 'Automatically iterates through all installed workflows sub-applications/projects/packages and imports their database entires. ' \
            'Note: Installed workflows packages are defined in mothra/settings.py via variable INSTALLED_APPS and begin with the string "workflows.". ' \
            'Auto import procedure does the following:\n' \
@@ -27,13 +27,21 @@ class Command(BaseCommand):
             default=False,
             help='No backup is created prior starting the import process.'
         ),
+        make_option('-a', '--ask',
+                    action="store_true",
+                    dest='ask',
+                    default=False,
+                    help='Ask to import packages which are marked not to be imported.'
+        ),
     )
 
     def handle(self, *args, **options):
-        auto_import_all_packages(self.stdout.write, options['nobackup'])
+        auto_import_all_packages(self.stdout.write, options['nobackup'], options['ask'])
         self.stdout.write('Auto import procedure finished.\n')
 
-def auto_import_all_packages(writeFunc, nobackup):
+def auto_import_all_packages(writeFunc, nobackup, ask):
+    if ask:
+        writeFunc('The procedure will interactively ask to import packages marked as not to be auto imported due to "--ask" option.\n')
     if nobackup:
         writeFunc('No backup will be created due to "--nobackup" option.\n')
     else:
@@ -82,7 +90,8 @@ def auto_import_all_packages(writeFunc, nobackup):
 
         if not imp:
             writeFunc('    AUTO_IMPORT_DB set to false in package\'s settings.\n')
-            continue
+            if not ask or not query_yes_no('    Do you want to import this package anyway?\n'):
+                continue
 
         for fileName in files:
             writeFunc('   Importing file "%s":\n' % fileName)
@@ -96,3 +105,35 @@ def auto_import_all_packages(writeFunc, nobackup):
 
     writeFunc('--------------------------------------------------------------------------------\n')
     return
+
+def query_yes_no(question, default=None):
+    """Ask a yes/no question via raw_input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+        It must be "yes" (the default), "no" or None (meaning
+        an answer is required of the user).
+
+    The "answer" return value is one of "yes" or "no".
+    """
+    valid = {"yes":True,   "y":True,  "ye":True,
+             "no":False,     "n":False}
+    if default == None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = raw_input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' " \
+                             "(or 'y' or 'n').\n")
