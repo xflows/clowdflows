ae6f2b2fe9438be974972c2f88dc67bbeae21ab2
matjaz <matjaz@jursic.si>
2013-03-10 07:40:48 +0100

    CrossBee: added standardized libraries for .net applications and adjusted to crossbee

---
+++ workflows/crossbee/library_gen.py
@@ -0,0 +1,7 @@
+# -----------------------------------------------------------------------------------------------------
+# WARNING: THIS IS AUTOMATICALLY GENERATED FILE, DO NOT EDIT IT MANUALLY AS YOU MAY LOOSE YOUR CHANGES!
+# -----------------------------------------------------------------------------------------------------
+
+from import_dotnet import *
+from serialization_utils import *
+
