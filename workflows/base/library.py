7c8daa1eb50dee7cc42c4c17a14c9b01b0cffc10
matjaz <matjaz@jursic.si>
2013-03-22 21:37:30 +0100

    Stopwatch: new base widget for timing processes.

--- workflows/base/library.py
+++ workflows/base/library.py
@@ -6,3 +6,18 @@ def base_concatenate_lists(input_dict):
     output_dict = {}
     output_dict['list']=new_list
     return output_dict
+
+def stopwatch(input_dict):
+    import time
+    inputTime = input_dict['time_in']
+    now = time.time()
+    if (isinstance(inputTime, float)):
+        elapsedTime = now - inputTime
+    else:
+        elapsedTime = None
+    output_dict = {}
+    output_dict['signal_out'] = input_dict['signal']
+    output_dict['time_out'] = now
+    output_dict['time_span'] = elapsedTime
+
+    return output_dict
