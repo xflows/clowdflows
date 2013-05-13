a209c539e7ac693cc7a9b168c36d626815aa476d
Anze Vavpetic <anze.vavpetic@ijs.si>
2013-05-13 14:29:46 +0200

    several decision support package fixes: attribute properties are now set in a separate dialog; added the option of setting a custom range for each attribute; unusable attributes (=discrete) are reported to the user; if no label attribute is available, the widgets use indices; minor refactoring

--- workflows/decision_support/interaction_views.py
+++ workflows/decision_support/interaction_views.py
@@ -6,5 +6,11 @@ Decision support interaction views.
 from django.shortcuts import render
 
 def decision_support_wsm(request, input_dict, output_dict, widget):
-    attributes = [att.name for att in input_dict['data'].domain.features]
-    return render(request, 'interactions/wsm.html', {'widget':widget, 'attributes':attributes})
+    from wsm import WeightedSumModel
+    data = input_dict['data']
+    model = WeightedSumModel(data)
+    attributes = sorted(model.ranges.items())
+    unusable = sorted(model.unusable)
+    return render(request, 'interactions/wsm.html', 
+                  {'widget' : widget, 'attributes' : attributes,
+                   'unusable_attributes' : unusable})
