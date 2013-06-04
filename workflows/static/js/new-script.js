6dcf7a3b55fff11156ce488edc07445b13ad5083
Janez K <janez.kranjc@gmail.com>
2013-06-04 09:32:22 +0200

    fixed bug when showing seconds and added readonly to the logging textarea

--- workflows/static/js/new-script.js
+++ workflows/static/js/new-script.js
@@ -7,6 +7,9 @@ function getCurrentTimeAsString() {
     if (minutes < 10)
     { minutes = "0" + minutes; }
 
+    if (seconds < 10)
+        { seconds = "0" + seconds; }
+
     return hours + ":" +minutes+":"+seconds;
 
 }
