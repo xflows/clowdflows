229326827f7642a32f9f8c3afd526bd27630f0bd
Janez K <janez.kranjc@gmail.com>
2013-11-14 15:50:18 +0100

    bugfix

--- workflows/visualization_views.py
+++ workflows/visualization_views.py
@@ -146,7 +146,7 @@ def pr_space_view(request,input_dict,output_dict,widget):
 def eval_bar_chart_view(request,input_dict,output_dict,widget):
     for i in input_dict['eval_results']:
         try:
-            i['accuracy'] = i['accuracy']*100
+            i['accuracy'] = i['accuracy']/100.0
         except:
             pass
     return render(request, 'visualizations/eval_bar_chart.html',{'widget':widget,'input_dict':input_dict,'output_dict':output_dict})
