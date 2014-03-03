ff9fe92ca3da13d69c655794c379a588147ffe75
Janez K <janez.kranjc@gmail.com>
2014-03-03 10:49:35 +0100

    removed unnecessary stuff

--- workflows/nl_toolkit/library.py
+++ workflows/nl_toolkit/library.py
@@ -24,23 +24,3 @@ def nl_toolkit_create_integers(input_dict):
     if input_dict['sort'].lower() == "true":
         intList.sort()
     return {'intList':intList}
-
-def nl_toolkit_sum_integers(input_dict):
-    intList = input_dict['intList']
-    return {'sum':sum(intList)}
-
-def nl_toolkit_pre_filter_integers(input_dict):
-    return input_dict
-
-def nl_toolkit_post_filter_integers(postdata,input_dict,output_dict):
-    intListOut = postdata['intListOut']
-    intList = []
-    for i in intListOut:
-        try:
-            intList.append(int(i))
-        except:
-            pass
-    return {'intList': intList}
-
-def nl_toolkit_pre_display_summation(input_dict):
-    return {}
