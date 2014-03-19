a6a1d32bb1539de3bce4ad8fc2901972e16d4810
Janez K <janez.kranjc@gmail.com>
2014-03-19 14:16:25 +0100

    test

--- workflows/tasks.py
+++ workflows/tasks.py
@@ -42,6 +42,6 @@ def runWidgetAsync(widget):
 
 @task()
 def runTest(stream,workflow):
-    for i in range(0,10):
+    for i in range(0,100):
         stream.execute(workflow=workflow)
     return "Done!"
