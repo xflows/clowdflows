1fd5038e7046d404341d616ab4049f480a73c1cb
Janez K <janez.kranjc@gmail.com>
2014-12-02 13:17:54 +0100

    api urls and api views

--- workflows/api_urls.py
+++ workflows/api_urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, include, url
-from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework import routers
+from workflows import api_views
+
+router = routers.DefaultRouter()
 
 urlpatterns = patterns('',
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^api-token-auth/?', 'rest_framework.authtoken.views.obtain_auth_token'),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 )
-
-urlpatterns = format_suffix_patterns(urlpatterns)
