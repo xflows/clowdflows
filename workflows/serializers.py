b42b7f267166debb28a1235a9e0aa5602c2c1ed4
Janez K <janez.kranjc@gmail.com>
2014-12-03 11:07:40 +0100

    started work on serializers

---
+++ workflows/serializers.py
@@ -0,0 +1,8 @@
+from workflows.models import Workflow
+from rest_framework import serializers
+#from rest_framework.reverse import reverse
+
+
+class WorkflowSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Workflow
