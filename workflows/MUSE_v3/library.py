0097e083debf9bd7752db215c271ad7a516cd4fc
vpodpecan <vid.podpecan@ijs.si>
2015-03-19 19:59:18 +0100

    new MUSE v3 widget(s)

--- workflows/MUSE_v3/library.py
+++ workflows/MUSE_v3/library.py
@@ -1,33 +1,23 @@
 import re
+import subprocess
+import logging
+try:
+    from ladon.clients.jsonwsp import JSONWSPClient
+except ImportError:
+    logging.warning('Ladon package not available, most of MUSE stuff will not work.')
 
-def MUSE_v3_create_integers(input_dict):
-    intStr = input_dict['intStr']
-    intList = []
-    for i in re.findall(r'\w+', intStr):
-        try:
-            intList.append(int(i))
-        except:
-            pass
-    if input_dict['sort'].lower() == "true":
-        intList.sort()
-    return {'intList':intList}
 
-def MUSE_v3_sum_integers(input_dict):
-    intList = input_dict['intList']
-    return {'sum':sum(intList)}
 
-def MUSE_v3_pre_filter_integers(input_dict):
-    return input_dict
+def MUSE_preprocessing(input_dict):
+    url = input_dict['url']
+    text = input_dict['text']
 
-def MUSE_v3_post_filter_integers(postdata,input_dict,output_dict):
-    intListOut = postdata['intListOut']
-    intList = []
-    for i in intListOut:
-        try:
-            intList.append(int(i))
-        except:
-            pass
-    return {'intList': intList}
+    import socket
+    socket.setdefaulttimeout(None)
 
-def MUSE_v3_pre_display_summation(input_dict):
-    return {}
+    cli = JSONWSPClient(url)
+    result = cli.preprocessing(text=text)
+    tokens = result.response_dict['result']
+    return {'tokens': tokens}
+#end
+
