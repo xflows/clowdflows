188a2eb66fbb3c58a676668840f8aa5f4a04fa45
Alain Shakour <ashakour@unistra.fr>
2015-10-21 16:36:44 +0200

    Improved attr. list & ilp pics

--- workflows/ilp/tertius/helper.py
+++ workflows/ilp/tertius/helper.py
@@ -11,9 +11,13 @@ def handle_files(fct_file_path,test_file_path, url):
     if not os.path.isfile(os.path.normpath(url + '.fct')) or not os.path.isfile(os.path.normpath(url + '.prd')):
         raise Exception('Prd or fct file missing')
 
-def create_attribute_list(attribute_str):
-    if attribute_str.strip():
-        attribute_list = attribute_str.split(',')
-        if attribute_list:       
-            return [str(e) for e in ' '.join(map(lambda field: '-d ' + str(field), attribute_list)).split(' ')]
+def create_attribute_list(attribute_str):        
+    if attribute_str: # e.g. : '  col1    5  eqb , col2 6 eqb   '
+        attribute_str = ' '.join(attribute_str.split()) # e.g. : 'col1 5 eqb , col2 6 eqb'
+        attribute_list = attribute_str.split(',') # e.g. : ['col1 5 eqb ',' col2 6 eqb']
+        if attribute_list:
+            #map & strip e.g. : ['-d col1 5 eqb','-d col2 6 eqb']
+            #join e.g. : '-d col1 5 eqb -d col2 6 eqb'
+            #split e.g. : ['-d','col1','5','eqb','-d','col2','6','eqb']
+            return ' '.join(map(lambda field: '-d ' + str(field).strip(), attribute_list)).split(' ')
     return ''
