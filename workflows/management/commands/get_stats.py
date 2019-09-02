697b36ebc976d1ba4ab726bda2fc4593422af080
Matej Martinc <matejmart@gmail.com>
2019-09-02 14:21:59 +0200

    making the code pretier

--- workflows/management/commands/get_stats.py
+++ workflows/management/commands/get_stats.py
@@ -13,8 +13,14 @@ class Command(BaseCommand):
     	year2 = int(d2.split('_')[0])
     	month1 = int(d1.split('_')[1])
     	month2 = int(d2.split('_')[1])
-    	print(month1, month2, year1, year2)
         return (year1 - year2) * 12 + month1 - month2
+
+    def format_date(self, num):
+    	if num < 10:
+    		num = str(0) + str(num)
+    	else:
+    		num = str(num)
+    	return num
 
     def handle(self, *args, **options):
     	cut_date = args[0]
@@ -24,45 +30,16 @@ class Command(BaseCommand):
         monthly_stats_active = defaultdict(int)
         daily_stats_active = defaultdict(int)
         counter = 0
-        today = str(datetime.today().year) + '_' + str(datetime.today().month) + '_' + str(datetime.today().day)
+        today = str(datetime.today().year) + '_' + self.format_date(datetime.today().month) + '_' + self.format_date(datetime.today().day)
         all_months = self.diff_month(today, cut_date)
 
 
         for user in users:
-        	
-        	joined_month = user.date_joined.month
-        	if joined_month < 10:
-        		joined_month = str(0) + str(joined_month)
-        	else:
-        		joined_month = str(joined_month)
-
-        	login_month = user.last_login.month
-        	if login_month < 10:
-        		login_month = str(0) + str(login_month)
-        	else:
-        		login_month = str(login_month)
-
-
-        	joined_day = user.date_joined.day
-        	if joined_day < 10:
-        		joined_day = str(0) + str(joined_day)
-        	else:
-        		joined_day = str(joined_day)
-
-        	login_day = user.last_login.day
-        	if login_day < 10:
-        		login_day = str(0) + str(login_day)
-        	else:
-        		login_day = str(login_day)
-
-
-
-        	ts_joined_d = str(user.date_joined.year) + '_' + joined_month + '_' + joined_day
-        	ts_active_d = str(user.last_login.year) + '_' + login_month + '_' + login_day
-        	ts_joined_m = str(user.date_joined.year) + '_' + joined_month
-        	ts_active_m = str(user.last_login.year) + '_' + login_day
-
-
+      
+        	ts_joined_d = str(user.date_joined.year) + '_' + self.format_date(user.date_joined.month) + '_' + self.format_date(user.date_joined.day)
+        	ts_active_d = str(user.last_login.year) + '_' + self.format_date(user.last_login.month) + '_' + self.format_date(user.last_login.day)
+        	ts_joined_m = str(user.date_joined.year) + '_' + self.format_date(user.date_joined.month)
+        	ts_active_m = str(user.last_login.year) + '_' + self.format_date(user.last_login.month)
 
         	if ts_joined_m > cut_date:
         		counter += 1
